bfadcfcda569784ce55068c8efd15b47e01bd90a
Maycon Bordin <mayconbordin@gmail.com>
2015-06-08 12:51:17 -0300

    Fixed erro ao editar aula modificando a data

--- sigai/app/Http/Controllers/AulaController.php
+++ sigai/app/Http/Controllers/AulaController.php
@@ -48,7 +48,7 @@ class AulaController extends Controller {
         try {
             $aula = AulaRepository::update($request->all(), $ucId, $turmaId, $data);
             
-            return redirect()->action('AulaController@mostrar', [$ucId, $turmaId, $data])
+            return redirect()->action('AulaController@mostrar', [$ucId, $turmaId, $aula->data])
 	                         ->with('success', Lang::get('turmas.saved'));
         } catch (NotFoundError $e) {
             return redirect()->action('TurmaController@mostrar', [$turmaId])
