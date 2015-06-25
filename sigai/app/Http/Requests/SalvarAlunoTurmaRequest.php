f3f3e3bfbb29fa380a536d3b783ac070d7d26ce1
Maycon Bordin <mayconbordin@gmail.com>
2015-06-25 08:18:15 -0300

    Modificada validação de vínculação de aluno a turmas, agora verificando valores permitidos para o status do aluno

--- sigai/app/Http/Requests/SalvarAlunoTurmaRequest.php
+++ sigai/app/Http/Requests/SalvarAlunoTurmaRequest.php
@@ -1,6 +1,7 @@
 <?php namespace App\Http\Requests;
 
 use App\Http\Requests\Request;
+use App\Models\Aluno;
 
 class SalvarAlunoTurmaRequest extends Request {
 
@@ -22,7 +23,10 @@ class SalvarAlunoTurmaRequest extends Request {
 	public function rules()
 	{
 		return [
-			'status'          => 'required|max:30',
+			'status' => 'required|max:30|in:'. implode(',', [
+                    Aluno::STATUS_NORMAL, Aluno::STATUS_CANCELADO, Aluno::STATUS_DISPENSADO, Aluno::STATUS_ENSINO_DISTANCIA,
+                    Aluno::STATUS_TRANCAMENTO_MATRICULA, Aluno::STATUS_TRANSFERIDO
+            ]),
 			'curso_origem_id' => 'required|integer|exists:cursos,id'
 		];
 	}
