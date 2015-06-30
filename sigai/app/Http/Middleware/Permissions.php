bec8a898c6f137aa5e2bf4e8d352227260fd2394
Maycon Bordin <mayconbordin@gmail.com>
2015-06-30 11:11:36 -0300

    Refatorado middleware de permissões

--- sigai/app/Http/Middleware/Permissions.php
+++ sigai/app/Http/Middleware/Permissions.php
@@ -1,15 +1,18 @@
 <?php namespace App\Http\Middleware;
 
+use App\Models\User;
 use App\Repositories\Contracts\TurmaRepositoryContract;
 use Closure;
 use \Auth;
 
 class Permissions
 {
+    protected $user;
     protected $turmaRepository;
 
     public function __construct(TurmaRepositoryContract $turmaRepository)
     {
+        $this->user = Auth::user();
         $this->turmaRepository = $turmaRepository;
     }
 
@@ -22,39 +25,25 @@ class Permissions
 	 */
 	public function handle($request, Closure $next)
 	{
-		$user  = Auth::user();
-		$route = $request->route();
-		
-		$permissions = $this->getPermissions($request);
-		$roles       = $this->getRoles($request);
-
-		foreach ($permissions as $permission) {
-		    if (strpos($permission, 'own') !== false) {
-		        if ($user->can($permission) && $this->hasOwnership($permission, $user, $route)) {
-		            return $this->response($request, $next, true);
-		        }
-		    } else if ($user->can($permission)) {
-		        return $this->response($request, $next, true);
-		    }
-		}
-
-		foreach ($roles as $role) {
-		    if ($user->hasRole($role)) {
-		        return $this->response($request, $next, true);
-	        }
-		}
-		
-		if (sizeof($roles) == 0 && sizeof($permissions) == 0) {
-		    return $this->response($request, $next, true);
-		}
+		if ($this->isFreeToGo($request) || $this->hasPermission($request) || $this->hasRole($request)) {
+            return $this->response($request, $next, true);
+        }
 
 		return $this->response($request, $next);
 	}
-	
+
+    /**
+     * Returns a response based on the authorized parameter.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure  $next
+     * @param  bool $authorized
+     * @return \Illuminate\Http\JsonResponse|\Illuminate\View\View
+     */
 	private function response($request, $next, $authorized = false)
 	{
 	    if (!$authorized) {
-		    if ($request->ajax()) {
+		    if ($request->ajax() || $request->wantsJson()) {
 			    return response()->json(['errors' => ['Unauthorized.']], 401);
 		    } else {
 			    return view('errors.403');
@@ -63,7 +52,78 @@ class Permissions
         
         return $next($request);
 	}
-	
+
+    /**
+     * Check if the user has any of the permissions necessary to access the resource. Note: the user only needs to have one of
+     * the permissions, not all of them to be authorized.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return bool
+     */
+    private function hasPermission($request)
+    {
+        $permissions = $this->getPermissions($request);
+        $route = $request->route();
+
+        foreach ($permissions as $permission) {
+            if (strpos($permission, '-own-') !== false) {
+                if ($this->user->can($permission) && $this->hasOwnership($permission, $this->user, $route)) {
+                    return true;
+                }
+            } else if ($this->user->can($permission)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Check if the user has any of the roles necessary to access the resource. Note: the user only needs to have one of
+     * the roles, not all of them to be authorized.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return bool
+     */
+    private function hasRole($request)
+    {
+        $roles = $this->getRoles($request);
+
+        foreach ($roles as $role) {
+            if ($this->user->hasRole($role)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Check if there are roles or permissions to be verified, if not then the user can proceed.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return bool
+     */
+    private function isFreeToGo($request)
+    {
+        $roles       = $this->getRoles($request);
+        $permissions = $this->getPermissions($request);
+
+        if (sizeof($roles) == 0 && sizeof($permissions) == 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Verify if the user has permission to see the resource.
+     *
+     * @param string $permission
+     * @param User $user
+     * @param \Illuminate/Routing/Route $route
+     * @return bool
+     */
 	private function hasOwnership($permission, $user, $route)
 	{
 	    switch ($permission)
@@ -97,7 +157,7 @@ class Permissions
     /**
      * Grab the permissions from the request
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return Array
      */
     private function getPermissions($request)
@@ -114,7 +174,7 @@ class Permissions
     /**
      * Grab the enabled roles from the request
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return Array
      */
     private function getRoles($request)
