b172e7a01b74e53fb1c53d037dd22ca1d37369c9
Maycon Bordin <mayconbordin@gmail.com>
2015-07-09 08:49:45 -0300

    Atualizado script do envoy

--- sigai/Envoy.blade.php
+++ sigai/Envoy.blade.php
@@ -1,33 +1,37 @@
 @servers(['web' => 'adrian@10.200.116.37'])
 
 @setup
-$app_dir = '/var/www/sigai/sigai'
+    $app_dir = '/var/www/sigai/sigai'
 @endsetup
 
 @macro('update')
-update:code
-update:db
+    update:code
+    update:db
 @endmacro
 
 @task('update:code', ['on' => 'web'])
-cd {{ $app_dir }};
-git pull origin master
-composer update
-bower update
-grunt
+    cd {{ $app_dir }};
+    php artisan down
+    git pull origin master
+    composer update
+    bower update
+    grunt
+    php artisan up
 @endtask
 
 @task('update:db', ['on' => 'web'])
-cd {{ $app_dir }};
-php artisan migrate
+    cd {{ $app_dir }};
+    php artisan migrate
 @endtask
 
 @task('merge:db', ['on' => 'web'])
-cd {{ $app_dir }};
-php artisan db:dump --merge-data /tmp/dump.sql
-php artisan db:drop --auto
-php artisan db:create
-php artisan migrate
-php artisan db:seed
-php artisan db:restore /tmp/dump.sql
+    cd {{ $app_dir }};
+    php artisan down
+    php artisan db:dump --merge-data /tmp/dump.sql
+    php artisan db:drop --auto
+    php artisan db:create
+    php artisan migrate
+    php artisan db:seed
+    php artisan db:restore /tmp/dump.sql
+    php artisan up
 @endtask
